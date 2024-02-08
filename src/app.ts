ce47b7643ab85baf3885b37305bb73e3c4520334
siffahim <siffahim25@gmail.com>
2024-02-08 17:11:13 +0600

    feat: :sparkles: eslint and prettier and husky and prettier setup

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import cors from "cors";
-import express from "express";
-const app = express();
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
 
 //parser
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json("Hello World");
+app.get('/', (req: Request, res: Response) => {
+  res.json('Working Successfully');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,19 @@
-import colors from "colors";
-import dotenv from "dotenv";
-
-import mongoose from "mongoose";
-import app from "./app";
-
-dotenv.config();
-
-const port = 5000;
+import colors from 'colors';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function run() {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lyhqa.mongodb.net/?retryWrites=true&w=majority`
-    );
-    console.log(colors.bgGreen("🚀 Database is connect successfully"));
+    //database connection
+    await mongoose.connect(config.database_url as string);
+    console.log(colors.bgGreen('🚀 Database is connect successfully'));
 
-    app.listen(port, () => {
-      console.log(colors.bgYellow(`Application Running on port ${port}`));
+    //app listening here
+    app.listen(config.port, () => {
+      console.log(
+        colors.bgYellow(`Application Running on port ${config.port}`)
+      );
     });
   } catch (err) {
     console.error(`🤢 Failed to connect Database ${err}`);
