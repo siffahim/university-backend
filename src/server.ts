e47c99d61f45b703518fefbfdf5ad8a680de8dea
siffahim <siffahim25@gmail.com>
2024-02-12 22:24:51 +0600

    feat: winston setup succesfully

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import colors from 'colors';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import { errorLogger, logger } from './shared/logger';
 
 async function run() {
   try {
     //database connection
     await mongoose.connect(config.database_url as string);
-    console.log(colors.green('🛢️  Database is connect successfully'));
+    logger.info(colors.green('🛢️  Database is connect successfully'));
 
     //app listening here
     const port =
@@ -16,10 +17,10 @@ async function run() {
         : parseInt(config.port!) || 5000;
 
     app.listen(port, '192.168.10.16', () => {
-      console.log(colors.yellow(`Application Running on port ${config.port}`));
+      logger.info(colors.yellow(`Application Running on port ${config.port}`));
     });
   } catch (err) {
-    console.error(`🤢 Failed to connect Database ${err}`);
+    errorLogger.error(`🤢 Failed to connect Database ${err}`);
   }
 }
 
